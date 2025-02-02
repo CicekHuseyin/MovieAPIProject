d0efe62377d688f9f6f691f597730c71d7e0bb77
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-02-02 16:15:09 +0300

    Add project files.

---
+++ src/projects/MovieProject.DataAccess/Deneme.cs
@@ -0,0 +1,16 @@
+﻿using MovieProject.Model.Entities;
+
+namespace MovieProject.DataAccess;
+
+public class Deneme
+{
+    public void Dene()
+    {
+        Movie movie = new Movie()
+        {
+            Id = Guid.NewGuid(),
+            CategoryId = 1,
+            CreatedTime = DateTime.Now
+        };
+    }
+}
---
+++ src/projects/MovieProject.Model/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using Core.DataAccess.Entities;
+
+namespace MovieProject.Model.Entities;
+
+public sealed class Category : Entity<int>
+{
+
+    public Category()
+    {
+        Name = string.Empty;
+        Movies = new List<Movie>();
+    }
+    public string Name { get; set; }
+
+    public ICollection<Movie> Movies { get; set; }
+}
+
+
+
+
+
