7f53f00207b47194fe58314eac68cc1d205e686e
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-03-03 16:35:48 +0300

    JWT Helper kodları yazıldı.

---
+++ src/core/Core.Security/JWT/TokenOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Security.JWT;
+
+public class TokenOptions
+{
+    public int AccessTokenExpiration { get; set; }
+    public string Audience { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string SecurityKey { get; set; } = string.Empty;
+}
